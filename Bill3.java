6ed55fe2b10a4add663727ceec25ee6bbc739a77
yameli2 <yam.elimelech@post.runi.ac.il>
2024-11-07 08:56:14 +0200

    Bill3

--- Bill3.java
+++ Bill3.java
@@ -7,6 +7,6 @@ public class Bill3 {
 		String name3 = args[2];
 		int bill = Integer.parseInt(args[3]);
 		double bill2 = Math.ceil(bill/3.0);
-		System.out.println("Dear "+ name3 +", "+ name2 +" and "+ name1 +": pay "+ bill2 +" Shekels each.");
+		System.out.println("Dear "+ name3 +", "+ name2 +", and "+ name1 +": pay "+ bill2 +" Shekels each.");
 	}
 }
